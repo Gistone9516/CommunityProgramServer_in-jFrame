75f794c0cc4ffa0c845d65c72aa6230f050e6608
Gistone9516 <65705919+Gistone9516@users.noreply.github.com>
2022-05-11 17:04:45 +0900

    Create ArrayOfObject.java

---
+++ ArrayOfObject.java
@@ -0,0 +1,18 @@
+package CServer;
+
+public class ArrayOfObject {
+	String department;
+	int year;
+	String name;
+	String sortation;
+	String classnum;
+	
+	ArrayOfObject(String depart, int yea, String nam, String sort, String num){
+		this.department = depart;
+		this.year = yea;
+		this.name = nam;
+		this.sortation = sort;
+		this.classnum = num;
+	}
+	
+}
